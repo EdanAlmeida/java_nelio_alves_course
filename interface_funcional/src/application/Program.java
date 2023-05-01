eb930af9fb1986cc4ef00390b4a1246235d1ddd9
EdanAlmeida <edluisalmeida@gmail.com>
2023-05-01 19:48:00 -0300

    interface funcional e predicate

---
+++ interface_funcional/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*Interface Funcional => É uma interface que possui um único método abstrato. Suas
+		implementações serão tratadas como expressões lambda.*/
+		
+//		public class MyComparator implements Comparator<Product> {
+//			@Override
+//			public int compare(Product p1, Product p2) {
+//			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+//			}
+//			}
+		
+//		public static void main(String[] args) {
+//			(...)
+//			list.sort(new MyComparator());
+
+		
+
+	}
+
+}
